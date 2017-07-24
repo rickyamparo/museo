6152194030e83d22e0a6819d385ad4d5c49617fe
RickyAmparo <rickyamparo@gmail.com>
2017-07-24 09:18:25 -0600

    create methods and tests for methods for artistid, museum_id and name in photograph class

--- lib/photograph.rb
+++ lib/photograph.rb
@@ -1,2 +1,24 @@
+require 'pry'
+
 class Photograph
+
+  attr_reader :photo, :id
+
+  def initialize(photo)
+    @photo = photo
+    @id = 1
+  end
+
+  def artist_id
+    @photo[:artist_id]
+  end
+
+  def museum_id
+    @photo[:museum_id]
+  end
+
+  def name
+    @photo[:name]
+  end
+
 end
--- test/photograph_test.rb
+++ test/photograph_test.rb
@@ -4,4 +4,60 @@ require 'minitest/pride'
 require './lib/photograph'
 
 class PhotographTest < Minitest::Test
+
+  def test_it_can_create_new_class
+    photo = Photograph.new({
+                            name: "Rue Mouffetard, Paris (Boy with Bottles)",
+                            artist_id: 4,
+                            museum_id: 2,
+                            year: 1954
+                            })
+
+    assert_instance_of Photograph, photo
+  end
+
+  def test_it_has_an_id
+    photo = Photograph.new({
+                            name: "Rue Mouffetard, Paris (Boy with Bottles)",
+                            artist_id: 4,
+                            museum_id: 2,
+                            year: 1954
+                            })
+
+    assert_equal 1, photo.id
+  end
+
+  def test_it_has_an_artist_id
+    photo = Photograph.new({
+                            name: "Rue Mouffetard, Paris (Boy with Bottles)",
+                            artist_id: 4,
+                            museum_id: 2,
+                            year: 1954
+                            })
+
+    assert_equal 4, photo.artist_id
+  end
+
+  def test_it_has_a_museum_id
+    photo = Photograph.new({
+                            name: "Rue Mouffetard, Paris (Boy with Bottles)",
+                            artist_id: 4,
+                            museum_id: 2,
+                            year: 1954
+                            })
+
+    assert_equal 2, photo.museum_id
+  end
+
+  def test_it_has_a_name
+    photo = Photograph.new({
+                            name: "Rue Mouffetard, Paris (Boy with Bottles)",
+                            artist_id: 4,
+                            museum_id: 2,
+                            year: 1954
+                            })
+
+    assert_equal "Rue Mouffetard, Paris (Boy with Bottles)", photo.name
+  end
+
 end
